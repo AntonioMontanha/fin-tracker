2332e750df64d003f4763436cec07ab7e7822568
Antonio Montanha <ammontanha@outlook.com>
2017-06-23 22:56:20 +0000

    Fixing the problem with signout

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
